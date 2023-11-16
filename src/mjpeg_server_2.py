c29ed03019fc312b9fc2177bee41c3bed0e4efe3
Henrique Povoa <hpovoa@idmind.pt>
2023-11-16 17:59:49 +0000

    fixed issue with camera driver that was preventing higher resolutions from working

--- src/mjpeg_server_2.py
+++ src/mjpeg_server_2.py
@@ -19,7 +19,7 @@ PAGE = """\
 </head>
 <body>
 <h1>Picamera2 MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="640" height="480" />
+<img src="stream.mjpg" width="1536" height="864" />
 </body>
 </html>
 """
@@ -82,7 +82,8 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+# picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+picam2.configure(picam2.create_video_configuration(main={"size": (1536, 864)}))
 output = StreamingOutput()
 picam2.start_recording(MJPEGEncoder(), FileOutput(output))
 
