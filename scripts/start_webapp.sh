4cf18f9a896935c752863c8bcb771c6ab4f4cfed
LucasVGon <lucasvgoncalves@tecnico.ulisboa.pt>
2026-01-06 15:51:15 +0000

    DEV: run start.sh first and fix pipewire (hardcoded solution, may need fixes)

--- scripts/start_webapp.sh
+++ scripts/start_webapp.sh
@@ -3,8 +3,12 @@
 export DISPLAY=:0
 export XDG_RUNTIME_DIR=/run/user/1000
 
+exec > /tmp/kiok.log 2>&1
+/bin/bash /home/idmind/elmo-v2/scripts/start.sh &
+
+
 # Wait for PipeWire and display to exist
-until pw-cli info core &>/dev/null; do
+until pw-cli info 0 &>/dev/null; do
   echo "Waiting for PipeWire..."
   sleep 2
 done
@@ -15,37 +19,32 @@ until xset q &>/dev/null; do
 done
 
 # set default microphone to virtual noise cancelled using PipeWire native commands
-MICROPHONE_NAME="alsa_input.usb-GeneralPlus_USB_Audio_Device-00.mono-fallback"
+MICROPHONE_NAME="alsa_input.usb-C-Media_Electronics_Inc._USB_PnP_Sound_Device-00.analog-mono"
 
 # Wait for the microphone to be available and get its node ID
-MICROPHONE_ID=""
-until [ -n "$MICROPHONE_ID" ]; do
-  MICROPHONE_ID=$(pw-dump Node | grep -B5 -A5 "$MICROPHONE_NAME" | grep '"id"' | head -1 | grep -oP ':\s*\K\d+')
-  [ -z "$MICROPHONE_ID" ] && sleep 1
-done
+MICROPHONE_ID="30"
+#until [ -n "$MICROPHONE_ID" ]; do
+# MICROPHONE_ID=$(pw-dump Node | grep -B5 -A5 "$MICROPHONE_NAME" | grep '"id"' | head -1 | grep -oP ':\s*\K\d+')
+#  [ -z "$MICROPHONE_ID" ] && sleep 1
+#done
 
 # Set as default source
-pw-cli set-default Audio/Source "$MICROPHONE_ID"
+pw-metadata -n default 0 default.audio.source "{ \"name\": \"$MICROPHONE_NAME\" }"
 
 # Set volume to 40% (0.4 in PipeWire)
-pw-cli set_param "$MICROPHONE_ID" Props '{ volume: 0.4 }'
+pw-cli set-param "$MICROPHONE_ID" Props '{ "volume": 0.4, "mute": false }'
 
 # Wait for the server
 until $(curl --output /dev/null --silent --head --fail http://localhost:8000); do
   sleep 1
 done
 
-
-# Determine which chromium executable to use
-CHROMIUM_CMD=""
-if [ -x /usr/bin/chromium ]; then
-  CHROMIUM_CMD="/usr/bin/chromium"
-elif [ -x /usr/bin/chromium-browser ]; then
-  CHROMIUM_CMD="/usr/bin/chromium-browser"
-else
-  echo "No suitable Chromium executable found."
-  exit 1
-fi
+CHROMIUM_CMD="/usr/bin/chromium"
 
 # Start the webapp
-$CHROMIUM_CMD --use-fake-ui-for-media-stream --password-store=basic --kiosk --app=http://localhost:8000?p=$RANDOM
+$CHROMIUM_CMD \
+  --disable-gpu \
+  --use-fake-ui-for-media-stream \
+  --password-store=basic \
+  --kiosk \
+  --app="http://localhost:8000?p=$RANDOM" &
