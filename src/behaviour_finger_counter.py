099e9dcf7b07329d1e2827e3783c011ded9d463e
AlsoBuilds <63322641+alsobuilds@users.noreply.github.com>
2026-05-29 15:40:33 +0100

    CHORE: FIle's formatting.

--- src/behaviour_finger_counter.py
+++ src/behaviour_finger_counter.py
@@ -78,13 +78,15 @@ class BehaviourFingerCounter:
 
         self.landmarker = mp.tasks.vision.HandLandmarker.create_from_options(
             mp.tasks.vision.HandLandmarkerOptions(
-                base_options=mp.tasks.BaseOptions(model_asset_path='/home/idmind/elmo-v2/src/hand_landmarker.task'),
+                base_options=mp.tasks.BaseOptions(
+                    model_asset_path="/home/idmind/elmo-v2/src/hand_landmarker.task"
+                ),
                 running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
                 num_hands=1,
                 min_hand_detection_confidence=0.3,
                 min_hand_presence_confidence=0.3,
                 min_tracking_confidence=0.3,
-                result_callback=update_result
+                result_callback=update_result,
             )
         )
 
