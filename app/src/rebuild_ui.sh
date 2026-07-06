e04a03a7a4dfb1308795ef596cc4d43d02634fe5
LVGoncalves <lgoncalves@idmind.pt>
2026-07-06 15:59:36 +0100

    DEV: Clear not need environment sourcing

--- app/src/rebuild_ui.sh
+++ app/src/rebuild_ui.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-source /home/luckin/Documents/elmo-v2-idmind/app/.venv/bin/activate
-
 #After resource additions, run this script to update the app
 #pyrcc5 resource.qrc -o resource_rc.py  # This is just a template, the resources in resources.qrc are unavailable
 
